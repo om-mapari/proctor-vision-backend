1f16139ac943304e48c357c6864c412ce9c60c1e
om mapari <mapariom05@gmail.com>
2023-03-09 16:09:19 +0530

    required package added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const cors = require("cors");
+const path = require("path");
+const uniqueId = require("uniqid");
+
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
