be617f628fca9106c363a662db0235a073795b45
om mapari <mapariom05@gmail.com>
2023-03-09 16:12:57 +0530

    added setinterval route

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const cors = require("cors");
 const path = require("path");
 const uniqueId = require("uniqid");
 
+var interval = 5000;
+
+
+
+/*  route to set new value interval  */
+app.get("/set_interval", (req, res) => {
+    interval = req.query.interval;
+    console.log('interval setted to ' + interval*1000);
+    res.send({ success: "Interval set" }).status(200).end();
+});
+
 
 
 app.listen(3000, () => {
